f8cbe30c02173e0e8abda7cd8e7e5748cb872e9c
Rojay <b00135409@mytudublin.ie>
2026-04-28 03:22:47 +0100

    Configure allowed hosts for Render deployment

--- pyfarm/settings.py
+++ pyfarm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^b2(u@iwziv7!8s9arcay)&exb3$)m4$!7))a$a#kr@4w@sy_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pyfarm.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
